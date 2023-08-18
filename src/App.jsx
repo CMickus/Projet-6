ee59b7d45fe6205881133c636ce8f63ecf5286b5
CMickus <mickuscolin@gmail.com>
2023-08-18 17:10:40 +0200

    correction du router bonne pratriques

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  BrowserRouter as Router, Routes, Route 
+  Routes, Route 
 } from "react-router-dom";
 //import root from "./index";
 import {Home} from "./components/main";
@@ -10,29 +10,12 @@ import {Logement} from "./pages/logements";
 import {Apropos} from "./pages/a-propos"
 import { Header } from './components/header';
 import {Footer } from './components/footer';
-import {ErrorPage} from './pages/error-page'
-/*
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <root />,
-    errorElement: <ErrorPage />
-  },  
-  {
-    path: "logements",
-    element: <Logement />,
-  },
-  {
-    path: "A-propos",
-    element: <Apropos />,
-  },
-]);*/
+import {ErrorPage} from './pages/error-page';
 
 //ici seront les routes du routings
 function App() {
   return (
-    //<React.StrictMode> <Route path="/" element={<Home/>}/>
-    <Router>
+    <>
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
@@ -41,9 +24,7 @@ function App() {
         <Route path="*" element={<ErrorPage/>}/>
       </Routes>
       <Footer/>
-    </Router>
-    //</React.StrictMode>
-    // je n'ai pas compris comment foncitonne les routes j'ai pleins d'informations qui se contre disent je suppose que " composant page c'est plutot Index ou Logement"
+    </>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 /*
 import Header from './components/header';
 import Main from './components/main';
@@ -12,7 +13,9 @@ import Apropos from './pages/a-propos'*/
 export const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />    
+    <BrowserRouter>
+      <App />    
+    </BrowserRouter>
   </React.StrictMode>
 );
 
