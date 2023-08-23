07e8e0c7f612729b1d08d3bbe5da14d7cd9ae993
CMickus <mickuscolin@gmail.com>
2023-08-23 08:44:49 +0200

    Style page erreur

--- src/pages/error-page.jsx
+++ src/pages/error-page.jsx
@@ -1,11 +1,12 @@
 import {Link} from 'react-router-dom';
+import style from './Error.module.css';
 
 export function ErrorPage() {
 
   return (
-    <div id="error-page">
-      <h1>404</h1>
-      <p>SOups! La page que vous demandez n'existe pas.</p>
+    <div id="error-page" className={style.content}>
+      <h1 className={style.title}>404</h1>
+      <p className={style.text}>Oups! La page que vous demandez n'existe pas.</p>
       <Link to="/">Retourner sur la page d'acceuil</Link>
     </div>
   );
