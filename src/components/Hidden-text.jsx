6b94535a208353c629448e904fafe6b3634b71b1
CMickus <mickuscolin@gmail.com>
2023-08-24 08:38:44 +0200

    Ajout d'un commentaire dans hidden text

--- src/components/Hidden-text.jsx
+++ src/components/Hidden-text.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import logementsListe from '../logements.json';
 
-//info peut etre descriprition ou équipement donc logementsListe.info est logementListe.description ou .equipement
-//à voir avec l'id de la page...
-
+//en gros si c'est page a propos juste faire un switch sur les class pour faire apparaitre et disparaitre le texte fixe
+//pour le reste voir si on peut retirer des element react via ces fonctions eux sont généré automatiquement
 export function HiddenText(info){
     info = info || null
     if (window.location == "http://localhost:3000/A-propos"){
