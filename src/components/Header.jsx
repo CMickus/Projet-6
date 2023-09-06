4bef34f83fd598af015da1d8a71d797d2c5993ae
CMickus <mickuscolin@gmail.com>
2023-09-06 07:53:33 +0200

    correction du style des navlink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../index.css';
 import style from './Header.module.css'
 import logo from './images/LOGO.jpg'
 
 
 /*Navlink plutot que Link permet un changement du visuel si on est sur la page sur lequel le lien mène ( a travailler)*/
-export function Header(){
+export function Header() {
     return (
         <header className={style.headerstyle}>
-            <img src={logo} alt="logo de Kasa" className={style.logo}/>
+            <img src={logo} alt="logo de Kasa" className={style.logo} />
             <div className={style.nav}>
-                <NavLink to="/" className={`
-                default
-                $style.link
-                $({isActive, isPending})=>isPending ? "pending" : isActive ? "link" : ""`}>Acceuil</NavLink>
-                <NavLink to="/A-propos" className={`
-                default
-                $style.link
-                $({isActive, isPending})=>isPending ? "pending" : isActive ? "active" : ""`}>À propos</NavLink>
+                <NavLink
+                    to="/"
+                    className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : style.link}>
+                    Acceuil
+                </NavLink>
+                <NavLink to="/A-propos" className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "active" : style.link}>
+                    À propos
+                </NavLink>
             </div>
         </header>
     )
 }
 
-export default {Header}
+export default { Header }
